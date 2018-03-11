30009c82ae668268c749fdaf8ab36e3159626f7f
Scotty Eckenthal <scott.eckenthal@gmail.com>
2018-03-11 19:06:11 -0400

    Adds bashrc / zshrc

--- config/shell/clean.sh
+++ config/shell/clean.sh
@@ -2,3 +2,5 @@
 
 [[ -e ~/.vimrc ]] && unlink ~/.vimrc
 [[ -e ~/.pathogenrc]] && unlink ~/.pathogenrc
+[[ -e ~/.bashrc]] && unlink ~/bashrc.
+[[ -e ~/.zshrc]] && unlink ~/.zshrc
--- config/shell/init.sh
+++ config/shell/init.sh
@@ -5,3 +5,5 @@ SCRIPTPATH="$( cd "$( dirname $0 )" ; pwd )"
 # Symlink vimrc
 ln -sf $SCRIPTPATH/vimrc ~/.vimrc
 ln -sf $SCRIPTPATH/pathogenrc ~/.pathogenrc
+ln -sf $SCRIPTPATH/bashrc ~/.bashrc
+ln -sf $SCRIPTPATH/zshrc ~/.zshrc
