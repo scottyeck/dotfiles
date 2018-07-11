d404564d09706c30c10b15ab5d7568bd2bfa710f
Scotty Eckenthal <scott.eckenthal@gmail.com>
2018-07-11 14:52:20 -0400

    feat: adds git alias cleanup

--- config/shell/clean.sh
+++ config/shell/clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+function removeGitAliases () {
+  git config --get-regexp alias | awk '{print $1}' | xargs -I {} git config --global --unset {}
+}
+
 function maybeUnlink () {
   FILE=$1
   if [ -f $FILE ]; then
@@ -12,5 +16,7 @@ maybeUnlink ~/.vimrc
 maybeUnlink ~/.pathogenrc
 maybeUnlink ~/.bashrc
 maybeUnlink ~/.zshrc
+
+removeGitAliases
 
 rm -rf ~/git/me/bag-of-tricks/.cache
