fa3cee2fbaccac5b9fcfc0668b8ba61bbaab5d22
Scotty Eckenthal <scott.eckenthal@gmail.com>
2019-01-30 12:36:43 -0500

    feat: Adds gbhist

--- config/shell/init.sh
+++ config/shell/init.sh
@@ -7,10 +7,3 @@ ln -sf $SCRIPTPATH/vimrc ~/.vimrc
 ln -sf $SCRIPTPATH/pathogenrc ~/.pathogenrc
 ln -sf $SCRIPTPATH/bashrc ~/.bashrc
 ln -sf $SCRIPTPATH/zshrc ~/.zshrc
-
-# git aliases
-
-# Lists the last 10 branches you worked on
-# Run `git lastworks`
-# https://gist.github.com/AvnerCohen/a25a69df4e13f469c4ee
-git config --global alias.lastworks "for-each-ref --count=10 --sort=-committerdate refs/heads/"
