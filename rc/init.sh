2e2d6d1c276197b3e6e28197b45a25b0c23e2949
Scotty Eckenthal <scott.eckenthal@gmail.com>
2019-08-18 21:58:09 -0400

    fix: ensure todo dir exists

--- rc/init.sh
+++ rc/init.sh
@@ -2,6 +2,9 @@
 
 SCRIPTPATH="$( cd "$( dirname $0 )" ; pwd )"
 
+# Ensure todo dir exists
+mkdir -p ~/.todo
+
 # Symlink vimrc
 ln -sf $SCRIPTPATH/vimrc ~/.vimrc
 ln -sf $SCRIPTPATH/pathogenrc ~/.pathogenrc
