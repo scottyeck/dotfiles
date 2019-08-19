ed8b98edf5196cb556bc6b5f6ee90da1889d69cd
Scotty Eckenthal <scott.eckenthal@gmail.com>
2019-08-18 22:08:35 -0400

    feat: Replace todo.txt init

--- rc/init.sh
+++ rc/init.sh
@@ -2,12 +2,8 @@
 
 SCRIPTPATH="$( cd "$( dirname $0 )" ; pwd )"
 
-# Ensure todo dir exists
-mkdir -p ~/.todo
-
 # Symlink vimrc
 ln -sf $SCRIPTPATH/vimrc ~/.vimrc
 ln -sf $SCRIPTPATH/pathogenrc ~/.pathogenrc
 ln -sf $SCRIPTPATH/bashrc ~/.bashrc
 ln -sf $SCRIPTPATH/zshrc ~/.zshrc
-ln -sf $SCRIPTPATH/todo.cfg ~/.todo/config
