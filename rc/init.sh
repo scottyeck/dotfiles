799870665177bb8fd2e8c730276bba26a34272c0
Scotty Eckenthal <scott.eckenthal@gmail.com>
2019-10-15 08:02:45 -0400

    chore: nvim support

--- rc/init.sh
+++ rc/init.sh
@@ -2,14 +2,19 @@
 
 SCRIPTPATH="$( cd "$( dirname $0 )" ; pwd )"
 
-# Symlink vimrc
+# Symlink homedir root rc's
 ln -sf $SCRIPTPATH/vimrc ~/.vimrc
 ln -sf $SCRIPTPATH/pathogenrc ~/.pathogenrc
 ln -sf $SCRIPTPATH/bashrc ~/.bashrc
 ln -sf $SCRIPTPATH/zshrc ~/.zshrc
 ln -sf $SCRIPTPATH/gitconfig ~/.gitconfig
 
-# Install pathogen
+CONFIGPATH=~/.config
 
+# Setup homedir config rc's
+mkdir -p $CONFIGPATH/nvim
+ln -sf $SCRIPTPATH/init.vim $CONFIGPATH/nvim/init.vim
+
+# Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
