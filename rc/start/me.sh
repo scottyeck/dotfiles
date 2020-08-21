1c7431af5ec6fba9d2ec950f82490bf21dfa6aff
Scotty Eckenthal <scott.eckenthal@gmail.com>
2020-08-21 14:36:12 -0400

    feat: [tmux] adds personal sesion config

---
+++ rc/start/me.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SESSION="me"
+SESSION_EXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Default session target, specifying no active window, which
+# will return us to the previously active window.
+SESSION_TARGET="$SESSION"
+
+# If session doesn't exist, we'll create a new one.
+if [ "$SESSION_EXISTS" = "" ]; then
+
+tmux new-session -d -s $SESSION
+
+tmux rename-window -t 0 'home'
+
+tmux new-window -t $SESSION:1 -n 'dots'
+tmux send-keys -t 'dots' 'cd dotfiles' C-m 'vim' C-m
+
+tmux new-window -t $SESSION:2 -n 'wiki'
+tmux send-keys -t 'wiki' 'cd wiki' C-m 'vim' C-m
+
+# Specify the window to which we want to attach
+SESSION_TARGET="$SESSION_TARGET:0"
+
+fi # // if session exists
+
+echo $SESSION_TARGET
+
+# Now that we know the session (either new or existing)
+# exists, attach to it.
+tmux attach-session -t "$SESSION_TARGET"
+
