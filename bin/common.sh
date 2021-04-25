a2f831946993436c6f76f92c7fb97ef705427b0b
Scotty Eckenthal <scott.eckenthal@gmail.com>
2021-04-25 13:23:49 -0400

    tmux proj mgmt cleanup

---
+++ bin/common.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ -z "$EXEC_NAME" ]; then
+  echo 'common.sh: ERROR Cannot initialize common lib for exec without var \"$EXEC_NAME\"'
+  return 1
+fi
+
+logGeneric () {
+  echo "$EXEC_NAME: $@"
+}
+
+logError () {
+  logGeneric "ERROR" "$@"
+}
+
