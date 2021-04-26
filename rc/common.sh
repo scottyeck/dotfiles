62e839f69f8dc2c560b0a35ee7c8e17b90c07ef5
Scotty Eckenthal <scott.eckenthal@gmail.com>
2021-04-26 12:32:10 -0400

    updating bootstrap

--- rc/common.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-SCRIPT_PATH="$( cd "$( dirname $0 )" ; pwd )"
-CONFIG_PATH=~/.config
-TODO_CONFIG_PATH=~/.todo
-VIM_CONFIG_PATH=~/.vim
-ALACRITTY_CONFIG_PATH=$CONFIG_PATH/alacritty
-KARABINER_CONFIG_PATH=$CONFIG_PATH/karabiner
-
