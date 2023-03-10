306b241a9f1585f669bed843b1f55be6c6a27d15
Scotty Eckenthal <scott.eckenthal@gmail.com>
2023-03-10 12:31:05 -0500

    comment.nvim explicit setup

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -228,7 +228,12 @@ require('lazy').setup({
   },
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',         opts = {} },
+  {
+    'numToStr/Comment.nvim',
+    config = function ()
+      require('Comment').setup()
+    end
+  },
 
   -- Fuzzy Finder (files, lsp, etc)
   { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
