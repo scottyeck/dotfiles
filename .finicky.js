a6ba06626748b4b041e134e6aad340a0a55f13df
Scotty Eckenthal <scott.eckenthal@gmail.com>
2023-04-17 13:33:46 -0400

    finicky setup

---
+++ .finicky.js
@@ -0,0 +1,27 @@
+// Use https://finicky-kickstart.now.sh to generate basic configuration
+// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+
+const profileNames = {
+  personal: "Default",
+  hen: "Profile 2",
+};
+
+module.exports = {
+  defaultBrowser: "Google Chrome",
+  handlers: [
+    {
+      match: /github.com\/comprehensiveio/,
+      browser: {
+        name: "Google Chrome",
+        profile: profileNames.hen,
+      },
+    },
+    {
+      match: /github.com\/scottyeck/,
+      browser: {
+        name: "Google Chrome",
+        profile: profileNames.personal,
+      },
+    },
+  ],
+};
