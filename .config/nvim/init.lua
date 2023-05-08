671798b6bb77a918f0d34b0ae0075620e437b70e
Scotty Eckenthal <scott.eckenthal@gmail.com>
2023-05-08 10:34:14 -0400

    chunkwise navigation

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -219,6 +219,35 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup{
+        on_attach = function(bufnr)
+              local gs = package.loaded.gitsigns
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+          
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+        end
+      }
+    end
+  },
 
   { -- Theme
     'haishanh/night-owl.vim',
