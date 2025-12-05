f677b78d20de0ba19f9973d25aec365a37293834
Scotty Eckenthal <scott.eckenthal@gmail.com>
2025-12-05 01:32:13 -0500

    nicer diagnostics

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -273,6 +273,16 @@ require('lazy').setup({
         end
       end)
     end,
+  },
+
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+        require("tiny-inline-diagnostic").setup()
+        vim.diagnostic.config({ virtual_text = false }) -- Disable Neovim's default virtual text diagnostics
+    end,
   }
 })
 
