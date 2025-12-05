4990c2b891f8e70cbcf627dd43f612be6bc73a5c
Scotty Eckenthal <scott.eckenthal@gmail.com>
2025-12-05 01:35:32 -0500

    git checkout

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -184,6 +184,47 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = '[S]earch [R]esume' })
+
+      -- Recent branches picker
+      vim.keymap.set('n', '<leader>gb', function()
+        local pickers = require('telescope.pickers')
+        local finders = require('telescope.finders')
+        local conf = require('telescope.config').values
+        local actions = require('telescope.actions')
+        local action_state = require('telescope.actions.state')
+
+        local branches = {}
+        local handle = io.popen("git branch --sort=-committerdate --format='%(refname:short)' 2>/dev/null | head -20")
+        if handle then
+          for line in handle:lines() do
+            table.insert(branches, line)
+          end
+          handle:close()
+        end
+
+        if #branches == 0 then
+          vim.notify('No branches found', vim.log.levels.WARN)
+          return
+        end
+
+        pickers.new({}, {
+          prompt_title = 'Recent Branches',
+          finder = finders.new_table({
+            results = branches,
+          }),
+          sorter = conf.generic_sorter({}),
+          attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+              actions.close(prompt_bufnr)
+              local selection = action_state.get_selected_entry()
+              if selection then
+                vim.cmd('Git checkout ' .. selection[1])
+              end
+            end)
+            return true
+          end,
+        }):find()
+      end, { desc = '[G]it [B]ranches [R]ecent' })
     end,
   },
 
