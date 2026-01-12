7d3e5d393b07a3d08f867ca356bd6956561c39c0
Scotty Eckenthal <scott.eckenthal@gmail.com>
2026-01-12 16:31:39 -0500

    Change spectre bindings to avoid collision

--- .config/nvim/lua/plugins/tools.lua
+++ .config/nvim/lua/plugins/tools.lua
@@ -105,7 +105,7 @@ return {
   {
     'nvim-pack/nvim-spectre',
     config = function()
-      vim.keymap.set('n', '<leader>S', '<cmd>lua require("spectre").toggle()<CR>', {
+      vim.keymap.set('n', '<leader>ss', '<cmd>lua require("spectre").toggle()<CR>', {
         desc = "Toggle Spectre"
       })
       vim.keymap.set('n', '<leader>sw', '<cmd>lua require("spectre").open_visual({select_word=true})<CR>', {
